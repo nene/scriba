3587cde39133130b6ae58e87dc830a1f2f5c50e2
Rene Saarsoo <nene@triin.net>
2014-01-17 09:54:53 +0200

    Convert JavaScript file from TABs to SPACEs.
    Accidentally used TABs as in previous project.

--- js/main.js
+++ js/main.js
@@ -1,77 +1,77 @@
 $(function() {
-	$("#slides").slidesjs({
-		width: 911,
-		height: 300,
-		pagination: {active: false, effect: "slide"},
-		navigation: {active: false, effect: "slide"}
-	});
+    $("#slides").slidesjs({
+        width: 911,
+        height: 300,
+        pagination: {active: false, effect: "slide"},
+        navigation: {active: false, effect: "slide"}
+    });
 
-	/**
-	 * A simple Markdown editor to edit the given `text`.
-	 */
-	function Editor(cfg) {
-		var field = $("<textarea/>").text(cfg.text);
-		field.css({
-			width: "100%",
-			height: cfg.height
-		});
-		
-		// Save button that switches back the old content
-		var button = $("<button/>").text("Salvesta");
-		
-		var editor = $("<div class='markdown-editor'>");
-		editor.append(field);
-		editor.append(button);
-		
-		button.click(cfg.save);
+    /**
+     * A simple Markdown editor to edit the given `text`.
+     */
+    function Editor(cfg) {
+        var field = $("<textarea/>").text(cfg.text);
+        field.css({
+            width: "100%",
+            height: cfg.height
+        });
 
-		this.getEl = function() {
-			return editor;
-		};
+        // Save button that switches back the old content
+        var button = $("<button/>").text("Salvesta");
 
-		this.getText = function() {
-			return field.val();
-		};
-	}
+        var editor = $("<div class='markdown-editor'>");
+        editor.append(field);
+        editor.append(button);
 
-	function getHtml(name, text, callback) {
-		$.ajax({
-			url: "/scriba/markdown",
-			data: {
-				name: name,
-				text: text
-			},
-			dataType: "html",
-			success: callback
-		});
-	}
+        button.click(cfg.save);
 
-	// Switch to editor when an editable area is clicked on.
-	$("article").on("click", ".editable", function() {
-		var editable = $(this);
-		var oldWidth = editable.width();
-		var oldHeight = editable.height();
-		var sectionName = $(this).data("name");
+        this.getEl = function() {
+            return editor;
+        };
 
-		$.ajax({
-			url: "/scriba/markdownSource",
-			data: {name: sectionName},
-			dataType: "text",
-			success: function(text) {
-				var editor = new Editor({
-					text: text,
-					height: oldHeight,
-					save: function() {
-						getHtml(sectionName, editor.getText(), function(html) {
-							editor.getEl().replaceWith(html);
-						});
-					}
-				});
+        this.getText = function() {
+            return field.val();
+        };
+    }
 
-				editable.replaceWith(editor.getEl());
-			}
-		});
+    function getHtml(name, text, callback) {
+        $.ajax({
+            url: "/scriba/markdown",
+            data: {
+                name: name,
+                text: text
+            },
+            dataType: "html",
+            success: callback
+        });
+    }
 
-	});
+    // Switch to editor when an editable area is clicked on.
+    $("article").on("click", ".editable", function() {
+        var editable = $(this);
+        var oldWidth = editable.width();
+        var oldHeight = editable.height();
+        var sectionName = $(this).data("name");
+
+        $.ajax({
+            url: "/scriba/markdownSource",
+            data: {name: sectionName},
+            dataType: "text",
+            success: function(text) {
+                var editor = new Editor({
+                    text: text,
+                    height: oldHeight,
+                    save: function() {
+                        getHtml(sectionName, editor.getText(), function(html) {
+                            editor.getEl().replaceWith(html);
+                        });
+                    }
+                });
+
+                editable.replaceWith(editor.getEl());
+            }
+        });
+
+    });
 
 });
