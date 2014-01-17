5ed2cfa644f46d8d764aa3fa285a36032f2969d9
Rene Saarsoo <nene@triin.net>
2014-01-17 11:01:12 +0200

    Mark all translatable strings in front page.

--- tpl/front-page.php
+++ tpl/front-page.php
@@ -1,123 +1,101 @@
 <div id="ordering-presentation">
-<h2>Tõlketöö tellimine:</h2>
+     <h2><?=_("Tõlketöö tellimine:")?></h2>
 
 <div id="slides">
   <div class="slide slide-1">
-  <p><span class="nr">1</span> <strong>Saada päring</strong>
-  Kirjuta meile, mis keelest mis keelde kirjalikku või suulist tõlget
-  soovid.
+  <p><span class="nr">1</span> <strong><?=_("Saada päring")?></strong>
+  <?=_("Kirjuta meile, mis keelest mis keelde kirjalikku või suulist tõlget soovid.")?>
   <a class="slidesjs-next slidesjs-navigation" href="#">&gt;</a></p>
   </div>
 
   <div class="slide slide-2">
-  <p><span class="nr">2</span> <strong>Teeme pakkumise</strong>
-  Vastame esimesel võimalusel ja teatame tõlketöö maksumuse ning tähtaja.
+  <p><span class="nr">2</span> <strong><?=_("Teeme pakkumise")?></strong>
+  <?=_("Vastame esimesel võimalusel ja teatame tõlketöö maksumuse ning tähtaja.")?>
   <a class="slidesjs-next slidesjs-navigation" href="#">&gt;</a></p>
   </div>
 
   <div class="slide slide-3">
-  <p><span class="nr">3</span> <strong>Kinnita tellimus</strong>
-  Kui oled hinnapakkumisega nõus, saada e-postiga tellimuse kinnitus.
+  <p><span class="nr">3</span> <strong><?=_("Kinnita tellimus")?></strong>
+  <?=_("Kui oled hinnapakkumisega nõus, saada e-postiga tellimuse kinnitus.")?>
   <a class="slidesjs-next slidesjs-navigation" href="#">&gt;</a></p>
   </div>
 
   <div class="slide slide-4">
-  <p><span class="nr">4</span> <strong>Tõlkimine</strong>
-  Pärast tõlke valmimist toimub materjali kujundamine, korrektuur ja ülevaatus toimetaja poolt.
+  <p><span class="nr">4</span> <strong><?=_("Tõlkimine")?></strong>
+  <?=_("Pärast tõlke valmimist toimub materjali kujundamine, korrektuur ja ülevaatus toimetaja poolt.")?>
   <a class="slidesjs-next slidesjs-navigation" href="#">&gt;</a></p>
   </div>
 
   <div class="slide slide-5">
-  <p><span class="nr">5</span> <strong>Valmis</strong>
-  Toimetame tõlgitud materjalid sinuni.
+  <p><span class="nr">5</span> <strong><?=_("Valmis")?></strong>
+  <?=_("Toimetame tõlgitud materjalid sinuni.")?>
   <a class="slidesjs-next slidesjs-navigation" href="#">&gt;</a></p>
   </div>
 
 </div>
 
-<p class="ask-price"><a href="<?=$base_url?>/hinnaparing" class="button">Küsi hinnapakkumist &nbsp; &gt;</a></p>
+<p class="ask-price"><a href="<?=$base_url?>/hinnaparing" class="button"><?=_("Küsi hinnapakkumist")?> &nbsp; &gt;</a></p>
 </div>
 
 
 <div class="col-1">
-  <h2>Teenused</h2>
+  <h2><?=_("Teenused")?></h2>
 
   <ul id="services">
     <li class="written-translation">
-      <h3><a href="<?=$base_url?>/kirjalik-tolge">Kirjalik tõlge</a></h3>
-      <p>Tõlke saadame tagasi võimalikult originaalilähedase
-      kujundusega. Keerukamate formaatide ja trükiste puhul pakume
-      ka küljendusteenust.
-      <a href="<?=$base_url?>/kirjalik-tolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/kirjalik-tolge"><?=_("Kirjalik tõlge")?></a></h3>
+      <p><?=_("Tõlke saadame tagasi võimalikult originaalilähedase kujundusega. Keerukamate formaatide ja trükiste puhul pakume ka küljendusteenust.")?>
+      <a href="<?=$base_url?>/kirjalik-tolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="oral-translation">
-      <h3><a href="<?=$base_url?>/suuline-tolge">Suuline tõlge</a></h3>
-      <p>Konverentsidel, koosolekutel kui ka muudel üritustel, kus
-      on oluline, et info jõuaks kõikide kuulajateni võimalikult
-      kiiresti.
-      <a href="<?=$base_url?>/suuline-tolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/suuline-tolge"><?=_("Suuline tõlge")?></a></h3>
+      <p><?=_("Konverentsidel, koosolekutel kui ka muudel üritustel, kus on oluline, et info jõuaks kõikide kuulajateni võimalikult kiiresti.")?>
+      <a href="<?=$base_url?>/suuline-tolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="referative-translation">
-      <h3><a href="<?=$base_url?>/referatiivne-tolge">Referatiivne tõlge</a></h3>
-      <p>Kui teil pole tarvis täielikku viimistletud tõlget, vaid
-      tarvis saada pigem kirjalik või suuline ülevaade teksti
-      sisust.
-      <a href="<?=$base_url?>/referatiivne-tolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/referatiivne-tolge"><?=_("Referatiivne tõlge")?></a></h3>
+      <p><?=_("Kui teil pole tarvis täielikku viimistletud tõlget, vaid tarvis saada pigem kirjalik või suuline ülevaade teksti sisust.")?>
+      <a href="<?=$base_url?>/referatiivne-tolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="proofreading">
-      <h3><a href="<?=$base_url?>/korrektuur-ja-toimetamine">Korrektuur ja toimetamine</a></h3>
-      <p>Teksti korrektuur tähendab teksti õigekeelsusnõuetega
-      vastavusse viimist ja trükivigade parandamist.  Toimetamine
-      sisaldab teksti keelelist ja ka sisulist parandamist.
-      <a href="<?=$base_url?>/korrektuur-ja-toimetamine">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/korrektuur-ja-toimetamine"><?=_("Korrektuur ja toimetamine")?></a></h3>
+      <p><?=_("Teksti korrektuur tähendab teksti õigekeelsusnõuetega vastavusse viimist ja trükivigade parandamist.  Toimetamine sisaldab teksti keelelist ja ka sisulist parandamist.")?>
+      <a href="<?=$base_url?>/korrektuur-ja-toimetamine"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="notarisation">
-      <h3><a href="<?=$base_url?>/notariaalne-kinnitamine">Notariaalne kinnitamine</a></h3>
-      <p>Tõlgitud dokumentide välisriigis kasutamiseks võidakse
-      nõuda notariaalselt kinnitatud tõlkeid.
-      <a href="<?=$base_url?>/notariaalne-kinnitamine">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/notariaalne-kinnitamine"><?=_("Notariaalne kinnitamine")?></a></h3>
+      <p><?=_("Tõlgitud dokumentide välisriigis kasutamiseks võidakse nõuda notariaalselt kinnitatud tõlkeid.")?>
+      <a href="<?=$base_url?>/notariaalne-kinnitamine"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="apostilling">
-      <h3><a href="<?=$base_url?>/apostillimine">Apostillimine</a></h3>
-      <p>Apostill näitab välisriigi ametnikule, et mis tahes
-      apostilliga kinnitatud dokumendi välja andnud asutus
-      eksisteerib ning tohib taolisi dokumente välja anda.
-      <a href="<?=$base_url?>/apostillimine">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/apostillimine"><?=_("Apostillimine")?></a></h3>
+      <p><?=_("Apostill näitab välisriigi ametnikule, et mis tahes apostilliga kinnitatud dokumendi välja andnud asutus eksisteerib ning tohib taolisi dokumente välja anda.")?>
+      <a href="<?=$base_url?>/apostillimine"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="legal-translation">
-      <h3><a href="<?=$base_url?>/oigustolge">Õigustõlge</a></h3>
-      <p>Õigustekstide tõlke alla kuuluvad tõlkijalt ülimat
-      täpsust ja korrektsust nõudvad õigusaktide ja muude
-      juriidiliste dokumentide tõlked.
-      <a href="<?=$base_url?>/oigustolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/oigustolge"><?=_("Õigustõlge")?></a></h3>
+      <p><?=_("Õigustekstide tõlke alla kuuluvad tõlkijalt ülimat täpsust ja korrektsust nõudvad õigusaktide ja muude juriidiliste dokumentide tõlked.")?>
+      <a href="<?=$base_url?>/oigustolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="technical-translation">
-      <h3><a href="<?=$base_url?>/tehnikatolge">Tehnikatõlge</a></h3>
-      <p>Tehnikatõlge on ala, kus saavad kokku tõlkijate
-      keelelised teadmised ja tehnikainimeste erialateadmised.
-      <a href="<?=$base_url?>/tehnikatolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/tehnikatolge"><?=_("Tehnikatõlge")?></a></h3>
+      <p><?=_("Tehnikatõlge on ala, kus saavad kokku tõlkijate keelelised teadmised ja tehnikainimeste erialateadmised.")?>
+      <a href="<?=$base_url?>/tehnikatolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="web-translation">
-      <h3><a href="<?=$base_url?>/kodulehe-tolge">Kodulehe tõlge</a></h3>
-      <p>Meil on laialdased kogemused kodulehtede tõlkimisel:
-      oleme teinud nii eesti- kui ka võõrkeelsele tekstile
-      korrektuuri, parandanud trüki- ja kirjavigu.
-      <a href="<?=$base_url?>/kodulehe-tolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/kodulehe-tolge"><?=_("Kodulehe tõlge")?></a></h3>
+      <p><?=_("Meil on laialdased kogemused kodulehtede tõlkimisel: oleme teinud nii eesti- kui ka võõrkeelsele tekstile korrektuuri, parandanud trüki- ja kirjavigu.")?>
+      <a href="<?=$base_url?>/kodulehe-tolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="manual-translation">
-      <h3><a href="<?=$base_url?>/kasutusjuhendi-tolge">Kasutusjuhendi tõlge</a></h3>
-      <p>Kasutusjuhendid erinevad teistest tekstidest oma
-      ülesehituse ja keelekasutuse poolest, mistõttu on vajalik,
-      et tõlkijal oleks selles vallas piisavalt kogemust, et
-      tagada parim tulemus.
-      <a href="<?=$base_url?>/kasutusjuhendi-tolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/kasutusjuhendi-tolge"><?=_("Kasutusjuhendi tõlge")?></a></h3>
+      <p><?=_("Kasutusjuhendid erinevad teistest tekstidest oma ülesehituse ja keelekasutuse poolest, mistõttu on vajalik, et tõlkijal oleks selles vallas piisavalt kogemust, et tagada parim tulemus.")?>
+      <a href="<?=$base_url?>/kasutusjuhendi-tolge"><?=_("Loe edasi")?></a></p>
     </li>
     <li class="movie-translation">
-      <h3><a href="<?=$base_url?>/filmitolge">Filmitõlge</a></h3>
-      <p>Kuigi filmiklipp võib lühike tunduda, võib tõlkimist
-      vajav tekst üsnagi pikk tulla ning seega soovitame selleks
-      tööks piisavalt aega varuda.
-      <a href="<?=$base_url?>/filmitolge">Loe edasi</a></p>
+      <h3><a href="<?=$base_url?>/filmitolge"><?=_("Filmitõlge")?></a></h3>
+      <p><?=_("Kuigi filmiklipp võib lühike tunduda, võib tõlkimist vajav tekst üsnagi pikk tulla ning seega soovitame selleks tööks piisavalt aega varuda.")?>
+      <a href="<?=$base_url?>/filmitolge"><?=_("Loe edasi")?></a></p>
     </li>
   </ul>
 
@@ -125,7 +103,7 @@
 
 <div class="col-2" id="client-quotes">
 
-  <h2>Kliendid meist</h2>
+  <h2><?=_("Kliendid meist")?></h2>
 
   <div class="client-quote">
     <blockquote>
